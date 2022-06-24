c235c332bc509a305447f1cef97114aeb14cc810
fossedihelm <ffossemo@redhat.com>
2022-06-24 17:39:34 +0200

    Add check kubevirt exists
    Signed-off-by: fossedihelm <ffossemo@redhat.com>

--- logs-collector/increase-verbosity.sh
+++ logs-collector/increase-verbosity.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e
 hco_name=$(kubectl get -n $HCO_NAMESPACE hco -o json | jq -r .items[0].metadata.name)
 if [[ -n "$hco_name" ]] ;
 then
@@ -8,7 +9,12 @@ then
 fi
 
 kubevirt_name=$(kubectl get -n $KUBEVIRT_NAMESPACE kubevirt -o json | jq -r .items[0].metadata.name)
-kubectl patch -n $KUBEVIRT_NAMESPACE kubevirt $kubevirt_name -p='[{"op": "add", "path": "/spec/configuration/developerConfiguration/logVerbosity", "value":{"virtLauncher":4,"virtHandler":4}}]' --type='json'
+if [[ -n "$kubevirt_name" ]] ;
+then
+  kubectl patch -n $KUBEVIRT_NAMESPACE kubevirt $kubevirt_name -p='[{"op": "add", "path": "/spec/configuration/developerConfiguration/logVerbosity", "value":{"virtLauncher":4,"virtHandler":4}}]' --type='json'
+  exit 0
+fi
+exit 1
 
 
 
